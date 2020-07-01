b4a4618c5f7538d8a994bffaaa8a4c088b9b7c0d
joaquin <j>
2020-06-30 21:59:08 -0700

    tldr

---
+++ tldr.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# https://github.com/isacikgoz/tldr/wiki/Installation
+
+curl -OL https://github.com/isacikgoz/tldr/releases/download/v0.5.0/tldr_0.5.0_linux_amd64.tar.gz
+tar xzf tldr_0.5.0_linux_amd64.tar.gz
