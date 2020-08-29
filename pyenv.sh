c199b76e2bf1252017cdd58efae4497e8af96d64
Joaquin Matres <j@j>
2020-08-29 14:58:16 -0700

    install 3.7 version of python

--- pyenv.sh
+++ pyenv.sh
@@ -3,5 +3,9 @@
 [ ! -d ~/.pyenv ] && git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
 # pyenv install 3.7.8
-pyenv install miniconda3-latest
-pyenv global miniconda3-latest
+# pyenv install miniconda3-latest
+# pyenv global miniconda3-latest
+
+
+pyenv install miniconda3-4.7.12
+pyenv global miniconda3-4.7.12
