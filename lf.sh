68b35aa47537a8282fe35aa2183b898e7d413c95
joaquin <j>
2020-11-22 12:00:09 -0800

    lf

--- lf.sh
+++ lf.sh
@@ -2,10 +2,8 @@
 # install lf for linux
 
 lf_bin=$(which lf) 2>&1 > /dev/null
-if [[ $? != 0 ]]; then
+if [ $? != 0 ]; then
   # go get -u github.com/gokcehan/lf
   mkdir -p ~/.local/bin
   curl -L https://github.com/gokcehan/lf/releases/download/r13/lf-linux-amd64.tar.gz | tar xzC ~/.local/bin
-fi 
-
-
+fi
