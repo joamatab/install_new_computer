9a7a1f0020510f94e0e0c24bb745dd04892f5392
joaquin <j>
2020-11-22 12:15:48 -0800

    fish

--- fish.sh
+++ fish.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 fish_bin=$(which fish) 2>&1 > /dev/null
+chsh -s $fish_bin
+
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
 if [[ ! -d $HOME/.local/share/omf ]]; then
   curl -L https://get.oh-my.fish | fish
 fi
-
-chsh -s $fish_bin
-curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
