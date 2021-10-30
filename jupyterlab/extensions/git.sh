bf049f552ced9757f1487e8566bb4b938e067d43
Joaquin Matres <j@j>
2021-10-30 09:04:17 -0700

    jupytervim

---
+++ jupyterlab/extensions/git.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# https://github.com/jwkvam/jupyterlab-vim
+
+pip install --upgrade jupyterlab jupyterlab-git
--- rust_packages.sh
+++ rust_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cargo install starship ripgrep tokei sd hyperfine fd du-dust dutree mcfly
+cargo install starship ripgrep tokei sd hyperfine fd du-dust dutree mcfly skim
 
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ajeetdsouza/zoxide/master/install.sh | sh
