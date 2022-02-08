f7ab4269fe1b0b56f713087c15fa04bc0d1ae27a
joaquin <j>
2022-02-08 07:43:44 -0800

    go install

--- go.sh
+++ go.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-wget https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz
-tar -C ~ -xzf go1.13.3.linux-amd64.tar.gz
+# wget https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz
+# tar -C ~ -xzf go1.13.3.linux-amd64.tar.gz
+
+git clone https://go.googlesource.com/go goroot
+cd goroot
+cd src
+./all.bash
