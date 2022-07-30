08261fb29205dd595d3c4e543bc9158e21309df7
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-07-30 14:59:15 -0700

    fix scripts

--- alacritty.sh
+++ alacritty.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone git@github.com:alacritty/alacritty.git
+[ ! -d alacritty ] && git clone git@github.com:alacritty/alacritty.git
 cd alacritty
 make install
 ln -sf $PWD/alacritty/target/release/alacritty ~/.local/bin
