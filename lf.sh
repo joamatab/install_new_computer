546c13b82872d42471b639b25969f13892b87cf0
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-12-06 14:52:49 -0800

    update lf

--- lf.sh
+++ lf.sh
@@ -4,9 +4,9 @@
 mkdir -p ~/.local/bin
 
 if [[ -d /Applications ]]; then
-  curl -L https://github.com/gokcehan/lf/releases/download/r26/lf-darwin-amd64.tar.gz | tar xzC ~/.local/bin
+  curl -L https://github.com/gokcehan/lf/releases/download/r27/lf-darwin-amd64.tar.gz | tar xzC ~/.local/bin
 
 else
-  curl -L https://github.com/gokcehan/lf/releases/download/r26/lf-linux-amd64.tar.gz | tar xzC ~/.local/bin
+  curl -L https://github.com/gokcehan/lf/releases/download/r27/lf-linux-amd64.tar.gz | tar xzC ~/.local/bin
 
 fi
