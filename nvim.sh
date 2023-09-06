ce01b128a739383bced45904a0355c8f9b7ce8d9
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-09-06 10:05:48 -0700

    update nvim

--- nvim.sh
+++ nvim.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-wget https://github.com/neovim/neovim/releases/download/v0.9.0/nvim-linux64.tar.gz
+wget https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
 ln -s "$PWD/nvim-linux64/bin/nvim" ~/.local/bin/
