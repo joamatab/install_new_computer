189f5a8b6614894b4cc3e6486a7dce7f13b0bfb0
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-10-17 13:24:20 -0700

    add github cli installer

---
+++ bash/githubcli2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+conda install gh --channel conda-forge -y
+# mamba install gh -y
+# curl https://github.com/cli/cli/releases/download/v2.37.0/gh_2.37.0_linux_amd64.deb -o gh_2.37.0_linux_amd64.deb
