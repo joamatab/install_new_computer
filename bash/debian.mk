bcfcdbe53a71b41921be781d9e7cca6278b5a9ac
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-10-19 08:37:46 -0700

    add swaylock

--- bash/debian.mk
+++ bash/debian.mk
@@ -35,7 +35,7 @@ install:
 		yarn \
 		zlib1g-dev
 sway:
-	sudo apt install sway waybar kanshi light alsa-utils slurp grim -y
+	sudo apt install sway waybar kanshi light alsa-utils slurp grim swaylock -y
 
 gui:
 	sudo apt install -y \
