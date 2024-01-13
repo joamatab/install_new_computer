a5d2440d3f2ca357df1dd2a613823052e3745374
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-01-13 09:05:18 -0800

    install miniconda only if does not exist

--- bash/python/miniconda.sh
+++ bash/python/miniconda.sh
@@ -1,3 +1,4 @@
 
 curl -L -O "https://repo.anaconda.com/miniconda/Miniconda3-latest-$(uname)-$(uname -m).sh"
-bash Miniconda3-latest-$(uname)-$(uname -m).sh -b
+
+[ ! -d ~/miniconda3 ] && bash Miniconda3-latest-$(uname)-$(uname -m).sh -b
