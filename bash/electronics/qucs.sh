a5cc0725e784483ed664cc270d2d25ad53b6f3a1
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-06-10 02:32:31 -0700

    install

--- bash/electronics/qucs.sh
+++ bash/electronics/qucs.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 
 sudo apt-get install ngspice build-essential git cmake qtbase5-dev qttools5-dev libqt5svg5-dev flex bison
-wget https://github.com/ra3xdh/qucs_s/releases/download/2.1.0/qucs-s-2.1.0.tar.gz
-tar xvfz qucs-s-2.1.0.tar.gz
-cd qucs-s-2.1.0
+wget https://github.com/ra3xdh/qucs_s/releases/download/24.2.1/qucs-s-24.2.1.tar.gz
+tar xvfz qucs-s-24.2.1.tar.gz
+cd qucs-s-24.2.1
 mkdir builddir
 cd builddir
 cmake ..
