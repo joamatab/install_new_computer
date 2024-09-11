d1259508982a653e67c29d778894b8bcdf864d48
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-09-11 16:18:41 -0700

    new ssh create

--- bash/ssh_create.sh
+++ bash/ssh_create.sh
@@ -4,7 +4,9 @@
 # create SSH key and open gitlab / github
 ########################################
 
-if [ ! -f $HOME/.ssh/id_rsa.pub ]; then
+export KEY_NAME="ed_key"
+
+if [ ! -f $HOME/.ssh/$KEY_NAME.pub ]; then
   echo 'Lets create a GIT credential and add it to http://github.com and/or gitlab.com'
   echo 'Go to gitlab and make sure you log in'
   read
@@ -16,21 +18,21 @@ if [ ! -f $HOME/.ssh/id_rsa.pub ]; then
   echo 'lets create an ssh key, so you can connect your computer with GIT without typing any password'
   read
   mkdir -p ~/.ssh
-  ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
+  ssh-keygen -f ~/.ssh/$KEY_NAME -t ed25519 -N ''
 
   echo 'Press any key to copy the key into the clipboard, then paste the key in gitlab and/or gitlab'
   read
 
   if [ -d /Applications ]; then
-    pbcopy < ~/.ssh/id_rsa.pub # copies key to clipboard
+    pbcopy < ~/.ssh/$KEY_NAME.pub # copies key to clipboard
     open 'http://gitlab.com/profile/keys'
     open 'https://github.com/settings/keys'
   elif [ -d /c ]; then
     echo 'Press any key to copy the key into the clipboard, then paste the key in gitlab'
     read
-    cat ~/.ssh/id_rsa.pub | clip # copies key to clipboard
+    cat ~/.ssh/$KEY_NAME.pub | clip # copies key to clipboard
   else
-    xclip -sel clip < ~/.ssh/id_rsa.pub
+    xclip -sel clip < ~/.ssh/$KEY_NAME.pub
     open 'http://gitlab.com/profile/keys'
     open 'https://github.com/settings/keys'
     # less ~/.ssh/id_rsa.pub
@@ -39,9 +41,9 @@ fi
 
 
 if [ -d /Applications ]; then
-  pbcopy < ~/.ssh/id_rsa.pub # copies key to clipboard
+  pbcopy < ~/.ssh/$KEY_NAME.pub # copies key to clipboard
 elif [ -d /c ]; then
-  cat ~/.ssh/id_rsa.pub | clip # copies key to clipboard
+  cat ~/.ssh/$KEY_NAME.pub | clip # copies key to clipboard
 else
-  xclip -sel clip < ~/.ssh/id_rsa.pub
+  xclip -sel clip < ~/.ssh/$KEY_NAME.pub
 fi
