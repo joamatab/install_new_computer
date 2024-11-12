9b496314d088d6f5b6add273233dac1d408e6025
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-11-12 08:33:03 -0800

    fix clipmenu

--- bash/clipmenu.sh
+++ bash/clipmenu.sh
@@ -3,6 +3,8 @@
 [ ! -d clipnotify ] && git clone https://github.com/cdown/clipnotify.git
 [ ! -d clipmenu ] && git clone https://github.com/cdown/clipmenu.git
 
+# sudo apt-get install libxfixes-dev
+
 cd clipnotify || exit
 make install
 cd clipmenu || exit
