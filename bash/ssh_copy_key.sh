dd02037abd89c4833906b5933e75020fa4c2e05a
Joaquin Matres <jmatres@google.com>
2024-12-13 10:16:30 -0800

    fix key

--- bash/ssh_copy_key.sh
+++ bash/ssh_copy_key.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # copy ssh key
 
+export KEY_NAME="id_ed25519"
+
 if [ -d /Applications ]; then
   pbcopy < ~/.ssh/id_rsa.pub # copies key to clipboard
 elif [ -d /c ]; then
   cat ~/.ssh/id_rsa.pub | clip # copies key to clipboard
 else
-  xclip -sel clip < ~/.ssh/id_rsa.pub
+  xclip -sel clip < ~/.ssh/$KEY_NAME.pub
 fi
