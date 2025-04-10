e159e28dc9722b680bd43260a76420bc05c3fe49
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-04-10 10:42:09 -0700

    add ubuntu

---
+++ bash/electronics/klayout/install_ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo apt-get update -y
+sudo apt-get install -y aptitude
+sudo aptitude install klayout
+
+wget https://www.klayout.org/downloads/Ubuntu-24/klayout_0.30.0-1_amd64.deb
+sudo dpkg -i klayout_0.30.0-1_amd64.deb
