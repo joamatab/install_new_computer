489320295d3fd1b41205b431b46b37cf8a457694
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-04-10 09:50:51 -0700

    latest ubuntu

--- bash/electronics/klayout/install_ubuntu_latest.sh
+++ bash/electronics/klayout/install_ubuntu_latest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt-get update -y
-sudo apt-get install -y qtbase5-dev gcc g++ make libz-dev qttools5-dev libqt5xmlpatterns5-dev qtmultimedia5-dev libqt5multimediawidgets5 libqt5svg5-dev ruby ruby-dev python3 python3-dev pip wget libgit2-dev
+sudo apt-get install -y klayout
 
 wget https://www.klayout.org/downloads/Ubuntu-24/klayout_0.30.0-1_amd64.deb
 sudo dpkg -i klayout_0.30.0-1_amd64.deb
