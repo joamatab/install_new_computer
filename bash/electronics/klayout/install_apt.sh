eb76895d3a4d605dd8a3a5b7f769d642233a6f49
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-04-10 09:40:13 -0700

    latest script

--- bash/electronics/klayout/install_apt.sh
+++ bash/electronics/klayout/install_apt.sh
@@ -3,8 +3,8 @@
 sudo apt-get update -y
 sudo apt-get install -y qtbase5-dev gcc g++ make libz-dev qttools5-dev libqt5xmlpatterns5-dev qtmultimedia5-dev libqt5multimediawidgets5 libqt5svg5-dev ruby ruby-dev python3 python3-dev pip wget
 
-wget https://www.klayout.org/downloads/source/klayout-0.27.8.tar.gz
-tar hzxvf klayout-0.27.8.tar.gz
-cd klayout-0.27.8
+wget https://www.klayout.org/downloads/source/klayout-0.30.0.tar.gz
+tar hzxvf klayout-0.30.0.tar.gz
+cd klayout-0.30.0
 ./build.sh -j4 -bin /usr/local/bin/klayout      # 4 cores
 pip3 install numpy
