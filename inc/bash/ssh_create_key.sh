c99516759c178787994d542ba2314ab624aa7296
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2026-02-19 12:15:12 -0800

    better message

--- inc/bash/ssh_create_key.sh
+++ inc/bash/ssh_create_key.sh
@@ -4,46 +4,66 @@
 # create SSH key and open gitlab / github
 ########################################
 
-export KEY_NAME="id_ed25519"
+KEY_NAME="id_ed25519"
+SSH_DIR="$HOME/.ssh"
+KEY_PATH="$SSH_DIR/$KEY_NAME"
 
-if [ ! -f $HOME/.ssh/$KEY_NAME.pub ]; then
-  echo 'Lets create a GIT credential and add it to http://github.com and/or gitlab.com'
-  echo 'Go to gitlab and make sure you log in'
-  read
-  if [! -d /c ]; then
-    open 'http://github.com'
-    open 'http://gitlab.com'
-  fi
-
-  echo 'lets create an ssh key, so you can connect your computer with GIT without typing any password'
-  read
-  mkdir -p ~/.ssh
-  ssh-keygen -f ~/.ssh/$KEY_NAME -t ed25519 -N ''
-
-  echo 'Press any key to copy the key into the clipboard, then paste the key in gitlab and/or gitlab'
+if [ -f "$KEY_PATH.pub" ]; then
+  echo "SSH key already exists at $KEY_PATH.pub"
+  echo "Copying public key to clipboard..."
+else
+  echo "==> Creating SSH key ($KEY_NAME)"
+  echo "    This lets you connect to GitHub/GitLab without typing a password."
+  echo ""
+  echo "    Make sure you are logged in to GitHub and GitLab in your browser."
+  echo "    Press Enter to open them now..."
   read
 
   if [ -d /Applications ]; then
-    pbcopy < ~/.ssh/$KEY_NAME.pub # copies key to clipboard
-    open 'http://gitlab.com/profile/keys'
-    open 'https://github.com/settings/keys'
-  elif [ -d /c ]; then
-    echo 'Press any key to copy the key into the clipboard, then paste the key in gitlab'
-    read
-    cat ~/.ssh/$KEY_NAME.pub | clip # copies key to clipboard
-  else
-    xclip -sel clip < ~/.ssh/$KEY_NAME.pub
-    open 'http://gitlab.com/profile/keys'
-    open 'https://github.com/settings/keys'
-    # less ~/.ssh/id_rsa.pub
+    open 'https://github.com'
+    open 'https://gitlab.com'
+  elif [ ! -d /c ]; then
+    xdg-open 'https://github.com' 2>/dev/null
+    xdg-open 'https://gitlab.com' 2>/dev/null
   fi
+
+  echo ""
+  echo "==> Generating SSH key..."
+  mkdir -p "$SSH_DIR"
+  ssh-keygen -f "$KEY_PATH" -t ed25519 -N ''
+  echo "    Key created at $KEY_PATH"
+
+  echo ""
+  echo "==> Starting ssh-agent and adding key..."
+  eval "$(ssh-agent -s)"
+  ssh-add "$KEY_PATH"
 fi
 
+# Copy public key to clipboard
+if [ -d /Applications ]; then
+  pbcopy < "$KEY_PATH.pub"
+elif [ -d /c ]; then
+  cat "$KEY_PATH.pub" | clip
+else
+  xclip -sel clip < "$KEY_PATH.pub"
+fi
+
+echo ""
+echo "==> Public key copied to clipboard!"
+echo ""
+echo "    Paste it into your GitHub/GitLab SSH key settings:"
+echo "    - https://github.com/settings/keys"
+echo "    - https://gitlab.com/-/user_settings/ssh_keys"
+echo ""
+echo "    Press Enter to open these pages..."
+read
 
 if [ -d /Applications ]; then
-  pbcopy < ~/.ssh/$KEY_NAME.pub # copies key to clipboard
-elif [ -d /c ]; then
-  cat ~/.ssh/$KEY_NAME.pub | clip # copies key to clipboard
-else
-  xclip -sel clip < ~/.ssh/$KEY_NAME.pub
+  open 'https://github.com/settings/keys'
+  open 'https://gitlab.com/-/user_settings/ssh_keys'
+elif [ ! -d /c ]; then
+  xdg-open 'https://github.com/settings/keys' 2>/dev/null
+  xdg-open 'https://gitlab.com/-/user_settings/ssh_keys' 2>/dev/null
 fi
+
+echo "Done! Test with: ssh -T git@github.com"
