b273b4dcfc8a9ce885472a6fb048a62130757b48
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2026-02-20 10:50:26 -0800

    fix install

--- inc/bash/fish.sh
+++ inc/bash/fish.sh
@@ -11,10 +11,7 @@ echo $fish_bin | sudo tee -a /etc/shells
 echo "    Changing default shell to Fish..."
 chsh -s $(which fish)
 
-echo "    Installing Fisher plugin manager..."
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-
-echo "    Installing Fisher..."
-curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+echo "    Installing Oh My Fish..."
+curl -L https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
 echo "==> Done! Restart your terminal to use Fish."
