b282a0f4ac3812f12097fabf1babc0704a4c0503
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2026-03-02 09:07:00 -0500

    update klayout

--- inc/electronics/klayout/install_mac.sh
+++ inc/electronics/klayout/install_mac.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [[ ! -f $HOME/Downloads/klayout.dmg ]]; then
-  curl https://www.klayout.org/downloads/MacOS/klayout-0.25.9-macOS-Mojave-1-Qt5124mp.dmg > ~/Downloads/klayout.dmg
+  curl https://www.klayout.org/downloads/MacOS/ARM64/arm64ST-klayout-0.30.6-macOS-Tahoe-1-qt5MP-RsysPsys.dmg > ~/Downloads/klayout.dmg
 fi
-
 open ~/Downloads/klayout.dmg
