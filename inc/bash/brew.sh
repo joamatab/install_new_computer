34ce485e37f4e549ad17751baff583099356a345
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2026-03-26 14:18:03 -0700

    add support from mac

--- inc/bash/brew.sh
+++ inc/bash/brew.sh
@@ -12,21 +12,37 @@ echo "==> Installing Homebrew and core packages..."
 running "checking homebrew install"
 brew_bin=$(which brew) 2>&1 > /dev/null
 if [[ $? != 0 ]]; then
-  # Homebrew installer requires admin privileges
-  if ! dseditgroup -o checkmember -m "$(whoami)" admin &>/dev/null; then
-    error "Homebrew installation requires an administrator account."
-    error "Your user ‘$(whoami)’ is not in the admin group."
-    error "Either run this from an admin account, or add your user to the admin group:"
-    error "  sudo dseditgroup -o edit -a $(whoami) -t user admin"
-    exit 2
-  fi
-  action "installing homebrew"
+  if dseditgroup -o checkmember -m "$(whoami)" admin &>/dev/null; then
+    # Admin user: standard install to /opt/homebrew
+    action "installing homebrew (admin)"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     if [[ $? != 0 ]]; then
       error "unable to install homebrew, script $0 abort!"
       exit 2
     fi
     eval "$(/opt/homebrew/bin/brew shellenv)"
+  else
+    # Non-admin user: install to home directory
+    action "installing homebrew in home directory (no admin required)"
+    HOMEBREW_PREFIX="$HOME/.homebrew"
+    mkdir -p "$HOMEBREW_PREFIX"
+    curl -fsSL https://github.com/Homebrew/brew/tarball/master | tar xz --strip-components 1 -C "$HOMEBREW_PREFIX"
+    if [[ $? != 0 ]]; then
+      error "unable to install homebrew to $HOMEBREW_PREFIX, script $0 abort!"
+      exit 2
+    fi
+    eval "$("$HOMEBREW_PREFIX/bin/brew" shellenv)"
+    action "adding homebrew shellenv to shell profiles"
+    BREW_SHELLENV="eval \"\$(${HOMEBREW_PREFIX}/bin/brew shellenv)\""
+    # Add to bash profile
+    if ! grep -qF "$HOMEBREW_PREFIX/bin/brew shellenv" "$HOME/.profile" 2>/dev/null; then
+      echo "$BREW_SHELLENV" >> "$HOME/.profile"
+    fi
+    # Add to zsh profile
+    if ! grep -qF "$HOMEBREW_PREFIX/bin/brew shellenv" "$HOME/.zprofile" 2>/dev/null; then
+      echo "$BREW_SHELLENV" >> "$HOME/.zprofile"
+    fi
+  fi
 else
   ok
   # Make sure we’re using the latest Homebrew
