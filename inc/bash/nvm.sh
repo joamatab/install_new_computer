e149d04eb3fc7dec46790f08c5b6bcf39469ba33
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2026-05-08 18:47:34 -0700

    use nvm

--- inc/bash/nvm.sh
+++ inc/bash/nvm.sh
@@ -4,4 +4,4 @@ echo "==> Installing nvm (Node Version Manager)..."
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 
-echo "==> Done! Restart your shell, then run 'nvm install node' to install Node.js."
+echo "==> Done! Restart your shell, then run 'nvm install 20' to install Node.js 20 LTS."
